0ca028d5675476bb2da1774a8c32f0369869f72e
Rodrigo Morais <morais.rm@gmail.com>
2015-06-29 17:19:38 -0300

    Creating a computed property in component to work with menus.

--- addon/components/ember-right-menu.js
+++ addon/components/ember-right-menu.js
@@ -6,6 +6,17 @@ export default Ember.Component.extend({
     attributeBindings: ['customId:id'],
     customId: '',
     title: '',
+    items: Ember.computed('menus', function() {
+        let _menus = this.get('menus'),
+            _items = Ember.Set.create();
+
+        _menus.forEach((_menu) => {
+            _items.add(_menu);
+        });
+
+
+        return _items.toArray();
+    }),
     actions: {
     }
 });
