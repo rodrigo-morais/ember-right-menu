cc8f6c5278dbdbae75b1490819f33d170d9b04be
Rodrigo Morais <morais.rm@gmail.com>
2015-06-30 17:32:23 -0300

    Creating all tests to builder of styles.

--- tests/unit/components/style-builder-test.js
+++ tests/unit/components/style-builder-test.js
@@ -13,3 +13,82 @@ test('verify if return styles blank when options are empty', (assert) =>{
         assert.equal(styles[_key], '');
     });
 });
+
+test('verify if return styles from default when options are empty', (assert) =>{
+    var builder = StyleBuilder.create(),
+        options = {},
+        optionsDefault = {
+            'menu': {
+                'background-color': '#F5F5F5',
+                'width': '15%'
+            },
+            'title': {
+                'border-bottom': 'solid 1px #f5da55',
+                'color': '#000000'
+            },
+            'menuItem':{
+                'color': '#8c8e87'
+            },
+            'menuItemSelected':{
+                'color': '#000000',
+                'background-color': '#FFFAF0',
+                'border-left': 'solid 2px #f5da55'
+            }
+        },
+        styles = '';
+    
+    styles = builder.build(optionsDefault, options);
+    
+    assert.equal(styles['menu'], 'background-color:#F5F5F5; width:15%');
+    assert.equal(styles['title'], 'border-bottom:solid 1px #f5da55; color:#000000');
+    assert.equal(styles['menuItem'], 'color:#8c8e87');
+    assert.equal(styles['menuItemSelected'], 'color:#000000; background-color:#FFFAF0; border-left:solid 2px #f5da55');
+});
+
+test('verify if return styles from options when options are informed', (assert) =>{
+    var builder = StyleBuilder.create(),
+        options = {
+            'menu': {
+                'background-color': '#FFFFFF',
+                'width': '40%'
+            },
+            'title': {
+                'border-bottom': 'solid 3px #000000',
+                'color': '#f5FF00'
+            },
+            'menuItem':{
+                'color': '#8c8e87'
+            },
+            'menuItemSelected':{
+                'color': '#000000',
+                'background-color': '#FFFAF0',
+                'border': 'solid 12px #FFda55'
+            }
+        },
+        optionsDefault = {
+            'menu': {
+                'background-color': '#F5F5F5',
+                'width': '15%'
+            },
+            'title': {
+                'border-bottom': 'solid 1px #f5da55',
+                'color': '#000000'
+            },
+            'menuItem':{
+                'color': '#8c8e87'
+            },
+            'menuItemSelected':{
+                'color': '#000000',
+                'background-color': '#FFFAF0',
+                'border-left': 'solid 2px #f5da55'
+            }
+        },
+        styles = '';
+    
+    styles = builder.build(optionsDefault, options);
+    
+    assert.equal(styles['menu'], 'background-color:#FFFFFF; width:40%');
+    assert.equal(styles['title'], 'border-bottom:solid 3px #000000; color:#f5FF00');
+    assert.equal(styles['menuItem'], 'color:#8c8e87');
+    assert.equal(styles['menuItemSelected'], 'color:#000000; background-color:#FFFAF0; border:solid 12px #FFda55');
+});
