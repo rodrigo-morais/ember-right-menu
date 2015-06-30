297caabc82a94a9af56dd29dea2a85a2c1d53bc9
Rodrigo Morais <morais.rm@gmail.com>
2015-06-29 21:20:52 -0300

    Adding the code to construct the style of menu dynamically.

--- addon/components/ember-right-menu.js
+++ addon/components/ember-right-menu.js
@@ -3,13 +3,42 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     tagName: 'div',
     optionsDefault: {
-        'background-color': '#000000'
+        'menu': {
+            'background-color': '#F5F5F5'
+        }
     },
+    options: {},
     title: '',
-    style: 'background-color: #F5F5F5',
     items: Ember.computed('menus', function() {
         let _menus = this.get('menus'),
-            _items = Ember.Set.create();
+            _items = Ember.Set.create(),
+            _options = {};
+
+        if(this.options){
+            if(typeof this.options === 'object'){
+                _options = Ember.$.extend({}, this.optionsDefault, this.options);
+            }
+            else{
+                _options = Ember.$.extend({}, this.optionsDefault, JSON.parse(this.options));
+            }
+        }
+        else{
+            _options = this.optionsDefault;
+        }
+
+        this.set('options', _options);
+
+        let menuStyle = '';
+        Object.keys(_options.menu).forEach((_option) =>{
+            if(menuStyle === ''){
+                menuStyle = _option + ':' + _options.menu[_option];
+            }
+            else{
+                menuStyle = menuStyle + ', ' + _option + ':' + _options.menu[_option];
+            }
+        });
+
+        this.set('menuStyle', menuStyle);
 
         _menus.forEach((_menu) => {
             _items.add(_menu);
